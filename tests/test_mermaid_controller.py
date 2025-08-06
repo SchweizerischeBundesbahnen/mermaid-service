938fb7b9573614c541cc7398371f81d70b62bfb4
Dzmitry Yurtsevich <dzmitry.yurtsevich@sbb.ch>
2025-08-06 19:24:36 +0200

    feat: endpoint of converting mermaid to svg with styling information added (#12)
    * feat: endpoint of converting mermaid to svg with styling information added
    * chore: revert to Python 3.11
    * chore: fix and extend tests

--- tests/test_mermaid_controller.py
+++ tests/test_mermaid_controller.py
@@ -2,6 +2,7 @@ import os
 import platform
 
 from fastapi.testclient import TestClient
+from unittest.mock import MagicMock, mock_open, patch
 
 from app.mermaid_controller import app
 
@@ -25,14 +26,63 @@ def test_convert():
     os.environ["MERMAID_SERVICE_VERSION"] = "test1"
     with TestClient(app) as test_client:
         result = test_client.post(
-            "/convert",
-            content='',
+            "/convert"
         )
         assert result.status_code == 400
         assert b"Empty request body" in result.content
+
         result = test_client.post(
             "/convert",
-            content='flowchart TD; A-->B;',
+            content=b'flowchart TD; A-->B;',
         )
         assert result.status_code == 200
         assert b"circle cx=\"50\" cy=\"50\" r=\"40\"" in result.content
+
+
+def test_convert_with_styling():
+    os.environ["MMDC"] = test_script_path
+    os.environ["MERMAID_SERVICE_VERSION"] = "test1"
+
+    with TestClient(app) as test_client:
+        result = test_client.post(
+            "/convert-with-styling",
+            data={},
+        )
+        assert result.status_code == 400
+        assert b"Check multipart form data of the request contains 'mmd' value and it's a string" in result.content
+
+        result = test_client.post(
+            "/convert-with-styling",
+            data={
+                "mmd": b"unknownDiagram"
+            },
+        )
+        assert result.status_code == 500
+        assert b"CalledProcessError" in result.content
+
+        result = test_client.post(
+            "/convert-with-styling",
+            data={
+                "mmd": b"sequenceDiagram; participant Participant 3; actor Actor 4; Participant 3->>Actor 4: message",
+                "css": b".actor { fill: #aaaaaa; }"
+            }
+        )
+        assert result.status_code == 200
+        assert b"circle cx=\"50\" cy=\"50\" r=\"40\"" in result.content
+        assert b".actor { fill: #aaaaaa; }" in result.content
+
+
+def test_wrong_mmd_cli():
+    os.environ["MMDC"] = "fake_path"
+    os.environ["MERMAID_SERVICE_VERSION"] = "test1"
+
+    with TestClient(app) as test_client:
+        result = test_client.post(
+            "/convert-with-styling",
+            data={
+                "mmd": b"sequenceDiagram; participant Participant 3; actor Actor 4; Participant 3->>Actor 4: message",
+                "css": b".actor { fill: #aaaaaa; }"
+            }
+        )
+        assert result.status_code == 500
+        assert b"FileNotFoundError" in result.content
